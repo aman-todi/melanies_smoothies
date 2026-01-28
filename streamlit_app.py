dc2e0df5a7ab6d507946679ba7ae875a0fae088b
Aman Todi <112562641+aman-todi@users.noreply.github.com>
2026-01-28 13:25:39 -0500

    a msg write added for debug purposes
    Display nutrition information for selected fruits in the smoothie.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,7 @@ if name_on_order and ingredients_list:
         ingredients_string += fruit + ' '
         # Display smoothiefruit nutrition information
         fruit_api_name = session.table("smoothies.public.fruit_options").select(col('search_on')).where(col('fruit_name')==fruit)
+        st.text(fruit_api_name)
         api_req_string = "https://my.smoothiefroot.com/api/fruit/" + fruit_api_name
         smoothiefroot_response = requests.get(api_req_string)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
